87c7c35747ef71e68eb20f6f25d0c80de213f3d7
Igor Khokhlov <eld2303@gmail.com>
2012-05-15 11:59:16 +0300

    Update classes/actions/ActionAjax.class.php

--- classes/actions/ActionAjax.class.php
+++ classes/actions/ActionAjax.class.php
@@ -423,6 +423,7 @@ class PluginLsgallery_ActionAjax extends ActionPlugin
             $oViewer = $this->Viewer_GetLocalViewer();
             $oViewer->Assign('aImages', $aImages);
             $oViewer->Assign('sType', 'new');
+            $oViewer->Assign('oUserCurrent', $this->oUserCurrent);
             $sTextResult = $oViewer->Fetch(Plugin::GetTemplatePath('lsgallery') . "block.stream_photo.tpl");
             $this->Viewer_AssignAjax('sText', $sTextResult);
         }
@@ -439,6 +440,7 @@ class PluginLsgallery_ActionAjax extends ActionPlugin
             $oViewer = $this->Viewer_GetLocalViewer();
             $oViewer->Assign('aImages', $aImages);
             $oViewer->Assign('sType', 'best');
+            $oViewer->Assign('oUserCurrent', $this->oUserCurrent);
             $sTextResult = $oViewer->Fetch(Plugin::GetTemplatePath('lsgallery') . "block.stream_photo.tpl");
             $this->Viewer_AssignAjax('sText', $sTextResult);
         }
