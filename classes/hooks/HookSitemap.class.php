14136339ab08dd97e184e414e2bf977f3af8cb2d
Golfersds <Golfersds@gmail.com>
2012-11-16 11:28:38 +0200

    Update annotation

--- classes/hooks/HookSitemap.class.php
+++ classes/hooks/HookSitemap.class.php
@@ -1,47 +1,36 @@
 <?php
 
-/* -------------------------------------------------------
-*
-*   LiveStreet Engine Social Networking
-*   Copyright © 2008 Mzhelskiy Maxim
-*
-* --------------------------------------------------------
-*
-*   Official site: www.livestreet.ru
-*   Contact e-mail: rus.engine@gmail.com
-*
-*   GNU General Public License, version 2:
-*   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-*
-* ---------------------------------------------------------
-*/
+/**
+ * PluginLsgallery_HookSitemap
+ *
+ * Hooks for generate sitemap
+ */
+class PluginLsgallery_HookSitemap extends Hook
+{
 
-class PluginLsgallery_HookSitemap extends Hook {
+    /**
+     * Цепляем обработчики на хуки
+     */
+    public function RegisterHook()
+    {
+        $this->AddHook('sitemap_index_counters', 'SitemapIndex');
+    }
 
-	/**
-	 * Цепляем обработчики на хуки
-	 *
-	 * @return void
-	 */
-	public function RegisterHook() {
-		$this->AddHook('sitemap_index_counters', 'SitemapIndex');
-	}
-
-	/**
-	 * Добавляем ссылку на Sitemap страниц в Sitemap Index
-	 *
-	 * @param array $aCounters
-	 * @return void
-	 */
-	public function SitemapIndex($aCounters) {
+    /**
+     * Добавляем ссылку на Sitemap страниц в Sitemap Index
+     *
+     * @param array $aCounters
+     */
+    public function SitemapIndex($aCounters)
+    {
         $aFilter = array(
-                'album_type' => array(
-                    'open' => true
-                ),
-                'not_empty' => true
-            );
-		$aCounters['albums'] = ceil($this->PluginSitemap_Album_GetCountAlbumsByFilter($aFilter) / Config::Get('plugin.sitemap.objects_per_page'));
-		$aCounters['photos'] = ceil($this->PluginSitemap_Image_GetCountImages($aFilter) / Config::Get('plugin.sitemap.objects_per_page'));
-	}
+            'album_type' => array(
+                'open' => true
+            ),
+            'not_empty' => true
+        );
+        $aCounters['albums'] = ceil($this->PluginLsgallery_Album_GetCountAlbumsByFilter($aFilter) / Config::Get('plugin.sitemap.objects_per_page'));
+        $aCounters['photos'] = ceil($this->PluginLsgallery_Image_GetCountImagesByFilter($aFilter) / Config::Get('plugin.sitemap.objects_per_page'));
+    }
 
 }
