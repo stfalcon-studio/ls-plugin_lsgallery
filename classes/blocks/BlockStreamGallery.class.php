4da4038430b9ea311ad1ea946b6132640944d33b
Igor Khokhlov <eld2303@gmail.com>
2013-02-01 16:43:55 +0200

    block steam to index

--- classes/blocks/BlockStreamGallery.class.php
+++ classes/blocks/BlockStreamGallery.class.php
@@ -5,16 +5,27 @@ class PluginLsgallery_BlockStreamGallery extends Block
 
     public function Exec()
     {
-        $aResult = $this->PluginLsgallery_Image_GetImagesNew(1, Config::Get('plugin.lsgallery.image_row'));
+        $aResult = $this->PluginLsgallery_Image_GetImagesBest(1, Config::Get('plugin.lsgallery.image_row'));
         if ($aResult['count']) {
             $aImages = $aResult['collection'];
             $oViewer = $this->Viewer_GetLocalViewer();
             $oViewer->Assign('aImages', $aImages);
-            $oViewer->Assign('sType', 'new');
-            $oViewer->Assign('oUserCurrent', $this->User_GetUserCurrent());
+            $oViewer->Assign('sType', 'best');
+            $oViewer->Assign('oUserCurrent', $this->oUserCurrent);
             $sTextResult = $oViewer->Fetch(Plugin::GetTemplatePath('lsgallery') . "block.stream_photo.tpl");
-            $this->Viewer_Assign('sStreamImages', $sTextResult);
+            $this->Viewer_Assign('sBestImages', $sTextResult);
         }
+
+//        $aResult = $this->PluginLsgallery_Image_GetImagesNew(1, Config::Get('plugin.lsgallery.image_row'));
+//        if ($aResult['count']) {
+//            $aImages = $aResult['collection'];
+//            $oViewer = $this->Viewer_GetLocalViewer();
+//            $oViewer->Assign('aImages', $aImages);
+//            $oViewer->Assign('sType', 'new');
+//            $oViewer->Assign('oUserCurrent', $this->User_GetUserCurrent());
+//            $sTextResult = $oViewer->Fetch(Plugin::GetTemplatePath('lsgallery') . "block.stream_photo.tpl");
+//            $this->Viewer_Assign('sStreamImages', $sTextResult);
+//        }
     }
 
 }
