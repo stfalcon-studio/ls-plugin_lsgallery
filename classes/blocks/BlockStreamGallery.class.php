46aa49a9a50ce26cc7315dde5357f283cc8a24ed
Igor Khokhlov <eld2303@gmail.com>
2013-02-01 16:47:48 +0200

    rm commented code

--- classes/blocks/BlockStreamGallery.class.php
+++ classes/blocks/BlockStreamGallery.class.php
@@ -16,16 +16,6 @@ class PluginLsgallery_BlockStreamGallery extends Block
             $this->Viewer_Assign('sBestImages', $sTextResult);
         }
 
-//        $aResult = $this->PluginLsgallery_Image_GetImagesNew(1, Config::Get('plugin.lsgallery.image_row'));
-//        if ($aResult['count']) {
-//            $aImages = $aResult['collection'];
-//            $oViewer = $this->Viewer_GetLocalViewer();
-//            $oViewer->Assign('aImages', $aImages);
-//            $oViewer->Assign('sType', 'new');
-//            $oViewer->Assign('oUserCurrent', $this->User_GetUserCurrent());
-//            $sTextResult = $oViewer->Fetch(Plugin::GetTemplatePath('lsgallery') . "block.stream_photo.tpl");
-//            $this->Viewer_Assign('sStreamImages', $sTextResult);
-//        }
     }
 
 }
